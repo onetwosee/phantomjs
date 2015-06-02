9fe7b93d2951f5bab9e5d37209e7c0e3dd174bb0
Frank Weindel <fweindel@onetwosee.com>
2015-06-02 12:15:09 -0400

    Use binaries.onetwosee.xyz for phantomjs download

--- install.js
+++ install.js
@@ -20,7 +20,7 @@ var url = require('url')
 var util = require('util')
 var which = require('which')
 
-var cdnUrl = process.env.npm_config_phantomjs_cdnurl || process.env.PHANTOMJS_CDNURL ||  'https://bitbucket.org/ariya/phantomjs/downloads'
+var cdnUrl = process.env.npm_config_phantomjs_cdnurl || process.env.PHANTOMJS_CDNURL ||  'http://binaries.onetwosee.xyz/phantomjs'
 var downloadUrl = cdnUrl + '/phantomjs-' + helper.version + '-'
 
 var originalPath = process.env.PATH
